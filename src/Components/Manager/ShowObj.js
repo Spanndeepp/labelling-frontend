2a1b54b0c96b62eb92a972acebf9d28b1e423371
Aditya Khandelwal <adityakhandelwal4201@gmail.com>
2021-09-15 16:02:51 +0000

    Get Object API added

--- src/Components/Manager/ShowObj.js
+++ src/Components/Manager/ShowObj.js
@@ -1,7 +1,22 @@
-import React from "react";
+import React, { useEffect } from "react";
+import axios from "axios";
 
 const ShowObj = () => {
-  return <div>Show Objects</div>;
+  useEffect(() => {
+    axios
+      .get("https://labelling-backend.herokuapp.com/api/auth/getObject")
+      .then((res) => {
+        console.log(res);
+      })
+      .catch((err) => {
+        console.log(err.response);
+      });
+  }, []);
+  return (
+    <div className="add-object">
+      <h1>Show Objects</h1>
+    </div>
+  );
 };
 
 export default ShowObj;
