3d1f855b2d2dc3b86fb459674f80b209dd15f843
Aditya Khandelwal <adityakhandelwal4201@gmail.com>
2021-09-22 03:14:05 +0000

    View Image complete

--- src/Components/Manager/ViewImage.js
+++ src/Components/Manager/ViewImage.js
@@ -1,4 +1,4 @@
-import React, { useRef, useState, useEffect } from "react";
+import React, { useRef, useState } from "react";
 import axios from "axios";
 
 const ViewImage = ({ currImage }) => {
@@ -20,49 +20,46 @@ const ViewImage = ({ currImage }) => {
     // console.log(ctx);
 
     setR(ctx);
-  };
 
-  useEffect(() => {
-    if (currImage) {
-      let file = currImage.replace(".jpg", ".txt");
-      //   console.log(file);
-      axios
-        .get(file)
-        .then((response) => {
-          const strArray = response.data.split(" ");
-          for (let i = 1; i < strArray.length; i = i + 4)
-            array.push({
-              x: parseFloat(strArray[i]),
-              y: parseFloat(strArray[i + 1]),
-              w: parseFloat(strArray[i + 2]),
-              h: parseFloat(strArray[i + 3]),
-            });
-          console.log(array);
-          if (array.length) {
-            const style = { borderColor: "black", borderWidth: 2 };
-            r.strokeStyle = style.borderColor;
-            r.lineWidth = style.borderWidth;
-            r.beginPath();
-            array.map((item) => {
-              console.log(
-                image.current.clientWidth,
-                image.current.clientHeight
-              );
-              return r.rect(
-                item.x * image.current.clientWidth,
-                item.y * image.current.clientHeight,
-                item.w * image.current.clientWidth,
-                item.h * image.current.clientHeight
-              );
-            });
-          }
-        })
-        .catch((error) => {
-          console.log(error.response);
-        });
-    }
-    //eslint-disable-next-line
-  }, [currImage]);
+    let file = currImage.replace(".jpg", ".txt");
+    //   console.log(file);
+    axios
+      .get(file)
+      .then((response) => {
+        const strArray = response.data.split(" ");
+        for (let i = 1; i < strArray.length; i = i + 4)
+          array.push({
+            x: parseFloat(strArray[i]),
+            y: parseFloat(strArray[i + 1]),
+            w: parseFloat(strArray[i + 2]),
+            h: parseFloat(strArray[i + 3]),
+          });
+        console.log(array);
+        if (array.length) {
+          r.strokeStyle = "black";
+          r.lineWidth = "2";
+          r.beginPath();
+          array.map((item) => {
+            // console.log(
+            //   item.x * image.current.clientWidth,
+            //   item.y * image.current.clientHeight,
+            //   item.w * image.current.clientWidth,
+            //   item.h * image.current.clientHeight
+            // );
+            return r.rect(
+              item.x * image.current.clientWidth,
+              item.y * image.current.clientHeight,
+              item.w * image.current.clientWidth,
+              item.h * image.current.clientHeight
+            );
+          });
+          r.stroke();
+        }
+      })
+      .catch((error) => {
+        console.log(error.response);
+      });
+  };
 
   return (
     <div className="image-area">
